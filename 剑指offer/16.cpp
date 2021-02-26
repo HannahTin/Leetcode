0351cbc0f827ad681ae896422fb298031c0caad1
HannahTin <menghantin@gmail.com>
2021-02-26 11:17:32 +0800

    数值的整数次方

---
+++ 剑指offer/16.cpp
@@ -0,0 +1,47 @@
+/*
+数值的整数次方
+
+题目描述：
+实现函数double Power(double base, int exponent)，求base的exponent次方。不得使用库函数，同时不需要考虑大数问题。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+// 总结：求 x^n最简单的方法是通过循环将n个x乘起来，时间复杂度为O(n)
+// 这里考虑快速幂的方法,时间复杂度为O(logn)，其中用右移1表示/2，用位与判断奇偶数
+// 本题还要尤其注意特殊值的处理。0的负指数幂，0的0次幂
+
+
+#include <iostream>
+using namespace std;
+
+double Pow(double x,int n){
+    if (n==0) return 1;
+    if (n==1) return x;
+    double res =Pow(x,n>>1);
+    res *=res;
+    if((n & 1) == 1){
+        res*=x;
+    }
+    return res;
+}
+
+double myPow(double x, int n) {
+        if(x ==0) return 0;
+        double res=1.0;
+        if(n<0){
+            res = 1/Pow(x,-n);
+        }
+        else if(n>0){
+            res = Pow(x,n);
+        }
+        return res;
+}
+
+int main() {
+    double res;
+    res = myPow(3,-1);
+    cout << res;
+    return 0;
+}

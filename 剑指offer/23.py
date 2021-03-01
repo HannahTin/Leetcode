ea4e57b0744522bd92bbd80f4c47eae49bc76984
HannahTin <menghantin@gmail.com>
2021-03-01 13:47:33 +0800

    环形链表

---
+++ 剑指offer/23.py
@@ -0,0 +1,72 @@
+'''
+首先看力扣141题：环形链表 
+
+给定一个链表，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+class ListNode:
+    def __init__(int x):
+        self.x = x
+        self.next = None
+def hasCycle(head: ListNode) -> bool:
+    slow,fast = head,head
+    while(fast and fast.next):
+        slow=slow.next
+        fast = fast.next.next
+        if(slow == fast):return True
+    return False
+'''
+然后看力扣142题：找出环形链表的入口节点
+
+题目描述：
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 
+如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+说明：不允许修改给定的链表。
+
+进阶：
+你是否可以使用 O(1) 空间解决此题？
+'''
+# 一个非常直观的思路是：我们遍历链表中的每个节点，
+# 并将它记录下来；一旦遇到了此前遍历过的节点，就可以判定链表中存在环。借助哈希表可以很方便地实现。
+'''
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode *> visited;
+        while (head != nullptr) {
+            if (visited.count(head)) {
+                return head;
+            }
+            visited.insert(head);
+            head = head->next;
+        }
+        return nullptr;
+    }
+};
+'''
+ def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return None
+        slow=faster=head
+        while faster and faster.next:
+            faster = faster.next.next
+            slow = slow.next
+            if faster == slow:
+                slow = head
+                while slow != faster:
+                    slow = slow.next
+                    faster = faster.next
+                return slow
+                break
+            
+        return None

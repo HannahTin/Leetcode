07b3e62fcaffdffe74bdb5f841f306c15f8c90c9
HannahTin <menghantin@gmail.com>
2021-03-08 16:45:59 +0800

    二叉树的最大深度

--- 二叉树/101_Maximum Depth of Binary Tree.cpp
+++ 二叉树/101_Maximum Depth of Binary Tree.cpp
@@ -1,3 +1,11 @@
+/**
+给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+**/
+
 using namespace std;
 #include <algorithm> 
 #include <queue>
@@ -10,6 +18,7 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
 // 递归写法
 int myDepth(TreeNode* node){
     if(node==nullptr) return 0;
@@ -22,7 +31,7 @@ int myDepth(TreeNode* node){
 int maxDepth(TreeNode* root) {
     return myDepth(root);
 }
-// 迭代写法
+// 迭代写法：层序遍历的层数即为最大深度
 int maxDepth(TreeNode* root) {
     if(root == nullptr) return 0;
     queue<TreeNode*> que;

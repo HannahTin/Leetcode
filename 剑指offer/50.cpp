36402d0c2bd6fc9d1f7db9ce5f7c2d9e3ce3feb0
HannahTin <menghantin@gmail.com>
2021-03-16 21:24:05 +0800

    第一个只出现一次的字符

---
+++ 剑指offer/50.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include<unordered_map>
+using namespace std;
+
+char firstUniqChar(string s) {
+        unordered_map<char,bool> dic;
+        for (char c:s){
+            dic[c] = dic.find(c)==dic.end();
+        }
+        for (char c:s){
+            if(dic[c]) return c;
+        }
+        return ' ';
+
+    }
+
+int main(){
+    cout<<firstUniqChar("abaccdeff") ; // b
+
+}

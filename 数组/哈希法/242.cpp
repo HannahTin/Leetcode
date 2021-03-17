227c570866af3130f0310946c91adbb2560352ae
HannahTin <menghantin@gmail.com>
2021-03-17 10:27:23 +0800

    有效的字母异位词

--- 数组/哈希法/242.cpp
+++ 数组/哈希法/242.cpp
@@ -6,6 +6,11 @@
 **/
 // 思路：字母异位即长度相同，各个字母出现的次数也相同只是位置不同。
 // 因此我们首先要判断s和t的长度，然后看t中的字母是否在s中出现过，并且出现次数是不是和s相同。
+
+#include <iostream>
+#include <string>
+using namespace std;
+
  bool isAnagram(string s, string t) {
         int record[26] = {0};
         for (int i = 0; i < s.size(); i++) {
@@ -23,4 +28,9 @@
         }
         // record数组所有元素都为零0，说明字符串s和t是字母异位词
         return true;
+}
+int main(){
+    cout<<isAnagram("anagram","nagaram"); // 1
+    cout<< isAnagram("rat","car"); //0
+
 }

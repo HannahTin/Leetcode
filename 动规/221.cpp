eca01abb008eff5f666b40b7ba1e541982c3f0a1
HannahTin <menghantin@gmail.com>
2021-03-25 12:39:47 +0800

    最大正方形的面积

--- 动规/221.cpp
+++ 动规/221.cpp
@@ -1,10 +1,10 @@
 /**
-2020/3/18 百度笔试原题 最大正方形
+2020/3/18 百度笔试原题 最大正方形的
 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
 **/
 #include <vector>
 using namespace std;
-
+// 只说一嘴就是dp[i][j]的意思就是以(i,j)为右下角的正方形的最大边长
 int maximalSquare(vector<vector<char>>& matrix) {
     if(matrix.size()==0 or matrix[0].size()==0) return 0;
     int maxside;

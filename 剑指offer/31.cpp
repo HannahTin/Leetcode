6e4a8d421f0322be12d4befb3a07aafb16263639
HannahTin <menghantin@gmail.com>
2021-04-02 16:27:28 +0800

    栈的压入、弹出序列

--- 剑指offer/31.cpp
+++ 剑指offer/31.cpp
@@ -9,6 +9,7 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 **/
 #include <vector>
+#include <stack>
 #include <iostream>
 using namespace std;
 bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {

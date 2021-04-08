5cf92774991d35f4cde8cc6fcaf2b79e78acb526
HannahTin <menghantin@gmail.com>
2021-04-08 16:45:42 +0800

    括号生成

---
+++ 回溯/22_Generate Parenthesis.cpp
@@ -0,0 +1,36 @@
+// 22. 括号生成
+// 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+// 输入：n = 3
+// 输出：["((()))","(()())","(())()","()(())","()()()"]
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+private:
+    vector<string> res;
+public:
+    void dfs(string str,int left,int right){ // 我觉得字符串的问题dfs也行 加个回溯操作也行，看传入的字符串是引用还是赋值了。赋值其实隐含了回溯操作。
+        if(left ==0 and right ==0) {
+            res.push_back(str);
+            return;
+        }
+        if(left == right){
+                //剩余左右括号数相等，下一个只能用左括号
+                dfs(str+"(",left-1,right);
+        }else if(left < right){
+                //剩余左括号小于右括号，下一个可以用左括号也可以用右括号
+                if(left > 0){
+                    dfs(str+"(",left-1,right);
+                }
+                dfs(str+")",left,right-1);
+        }
+
+
+
+    }
+    vector<string> generateParenthesis(int n) {
+        string str ="";
+        dfs(str,n,n);
+        return res;
+    }
+};

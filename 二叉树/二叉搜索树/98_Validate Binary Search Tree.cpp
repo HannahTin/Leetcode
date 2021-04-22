6a8dd77bc3795ee273465132bee6ac15fb650cdf
HannahTin <menghantin@gmail.com>
2021-04-22 11:49:00 +0800

    验证二叉搜索树

---
+++ 二叉树/二叉搜索树/98_Validate Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+/**
+98. 验证二叉搜索树
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+**/
+//  Definition for a binary tree node.
+// 中序遍历实现，因为bst的中序遍历是从小到大有序的
+#include <stack>
+#include <vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        long inorder = (long)INT_MIN - 1;
+        while (cur != NULL || !st.empty()) {
+            if (cur != NULL) { // 指针来访问节点，访问到最底层
+                st.push(cur); // 讲访问的节点放进栈
+                cur = cur->left;                // 左
+            } else {
+                cur = st.top(); // 从栈里弹出的数据，就是要处理的数据（放进result数组里的数据）
+                st.pop();
+                if(cur->val <= inorder) return false; 
+                inorder = cur->val;     // 中
+                cur = cur->right;               // 右
+            }
+        }
+        return true;
+
+    }
+};

407573f1d5ab0c4f48c07566c49a8e475cfede4d
HannahTin <menghantin@gmail.com>
2021-04-23 17:14:44 +0800

    二叉搜索树中的插入操作 (迭代版)

---
+++ 二叉树/二叉搜索树/701(2)_Insert into a Binary Search Tree.cpp
@@ -0,0 +1,38 @@
+// BST插入操作的迭代写法
+// 其实和插入的递归写法是一样的。注意二叉树递归的特性。
+#include <stack>
+#include <vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+private:
+    TreeNode* parent;
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == nullptr) {
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        TreeNode* curr = root;
+        TreeNode* parent;
+        while(curr!=nullptr){
+            parent = curr;
+            if(curr->val < val) curr = curr->right;
+            else if(curr->val > val) curr = curr->left;
+        }
+        TreeNode* node = new TreeNode(val);
+        if (val > parent->val) parent->right = node;
+        else parent->left = node;
+        return root;
+
+
+
+    }
+};

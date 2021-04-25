72642407c0998211e81deae28e6e7982d9563f4d
HannahTin <menghantin@gmail.com>
2021-04-25 11:51:25 +0800

    数据流中的第 K 大元素 (heapq)

---
+++ 二叉树/二叉搜索树/703_Kth Largest Element in a Stream.py
@@ -0,0 +1,33 @@
+'''
+数据流中的第 K 大元素
+设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第 k 大元素，不是第 k 个不同的元素。
+
+请实现 KthLargest 类：
+
+KthLargest(int k, int[] nums) 使用整数 k 和整数流 nums 初始化对象。
+int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 k 大的元素。
+'''
+import heapq
+# 维护一个小根堆
+class KthLargest:
+
+    def __init__(self, k: int, nums: list):
+        self.k=k
+        self.hp = [num for num in nums[:k]] # k个数入堆
+        heapq.heapify(self.hp)
+        for i in range(self.k,len(nums)):
+            heapq.heappush(self.hp,nums[i])
+            heapq.heappop(self.hp)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.hp,val)
+        if len(self.hp) > self.k:
+            heapq.heappop(self.hp)
+        return self.hp[0]
+        
+
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)

b17f552e133a971892c7e52035762f549a2e02c7
HannahTin <menghantin@gmail.com>
2021-04-25 20:12:59 +0800

    二叉树的最近公共祖先

--- 二叉树/236_ Lowest Common Ancestor of a Binary Tree.py
+++ 二叉树/236_ Lowest Common Ancestor of a Binary Tree.py
@@ -27,3 +27,32 @@ def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         if not left: return right
         if not right:return left
         return root
+# 下面给出c++写法，只要二叉树中节点都不相同，可以求出任意m个节点的最近公共祖先
+# 所有 Node.val 互不相同 。
+# p != q
+# p 和 q 均存在于给定的二叉树中。
+'''
+参考：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/er-cha-shu-de-zui-jin-gong-gong-zu-xian-by-leetc-2/​	
+时间复杂度：O(N)，其中 N 是二叉树的节点数。二叉树的所有节点有且只会被访问一次，因此时间复杂度为 O(N)。
+
+空间复杂度：O(N)，其中 NN 是二叉树的节点数。递归调用的栈深度取决于二叉树的高度，二叉树最坏情况下为一条链，此时高度为 N，因此空间复杂度为 O(N)O(N)。
+
+class Solution {
+public:
+    TreeNode* ans;
+    bool dfs(TreeNode* root, unordered_set<TreeNode*>& vec) {
+        if (root == nullptr) return false;
+        bool lson = dfs(root->left,vec);
+        bool rson = dfs(root->right, vec);
+        if ((lson && rson) || (vec.count(root) && (lson || rson))) {
+            ans = root;
+        } 
+        return lson || rson || vec.count(root);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        unordered_set<TreeNode*> vec ={p,q};
+        dfs(root,vec);
+        return ans;
+    }
+};
+'''

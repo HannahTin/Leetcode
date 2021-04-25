aeaca4ac766a4fb35136533d5791c7458d20cf78
HannahTin <menghantin@gmail.com>
2021-04-25 11:39:48 +0800

    删除二叉搜索树中的节点（递归）

--- 二叉树/二叉搜索树/450(2)_Delete in a Binary Search Tree.cpp
+++ 二叉树/二叉搜索树/450(2)_Delete in a Binary Search Tree.cpp
@@ -1,3 +1,14 @@
+/**
+450. 删除二叉搜索树中的节点
+给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+
+一般来说，删除节点可分为两个步骤：
+
+首先找到需要删除的节点；
+如果找到了，删除它。
+说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+**/
+// 递归版
 #include <stack>
 #include <vector>
 using namespace std;
@@ -44,6 +55,26 @@ public:
         if (pre->right && pre->right->val == key) {
             pre->right = deleteOneNode(cur);
         }
+        return root;
+    }
+};
+// 递归优化版
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) return root;
+        if (root->val == key) {
+            if (root->right == nullptr) { // 这里第二次操作目标值：最终删除的作用
+                return root->left; 
+            }
+            TreeNode *cur = root->right;
+            while (cur->left) {
+                cur = cur->left;
+            }
+            swap(root->val, cur->val); // 这里第一次操作目标值：交换目标值其右子树最左面节点。
+        }
+        root->left = deleteNode(root->left, key);
+        root->right = deleteNode(root->right, key);
         return root;
     }
 };

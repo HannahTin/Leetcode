f6b2ba4cdc568e6605636120df0884b87c787428
HannahTin <menghantin@gmail.com>
2021-05-07 10:40:16 +0800

    打家劫舍 III （树形dp入门款）

---
+++ 动规/打家劫舍/337_House Robber III.cpp
@@ -0,0 +1,71 @@
+/**
+337. 打家劫舍 III
+在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+
+计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+
+示例 1:
+
+输入: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+输出: 7 
+解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+**/
+using namespace std;
+#include <vector>
+#include <unordered_map>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+//  简单粗暴的后序遍历，但必须加上记忆化递归，否则会超时。
+// 时间复杂度：O(n)
+// 空间复杂度：O(n) 算上递推系统栈的空间
+class Solution {
+public:
+    unordered_map<TreeNode* , int> umap; // 记录计算过的结果
+    int rob(TreeNode* root) {
+        if (root == NULL) return 0;
+        if (root->left == NULL && root->right == NULL) return root->val;
+        if (umap[root]) return umap[root]; // 如果umap里已经有记录则直接返回
+        // 偷父节点
+        int val1 = root->val;
+        if (root->left) val1 += rob(root->left->left) + rob(root->left->right); // 跳过root->left
+        if (root->right) val1 += rob(root->right->left) + rob(root->right->right); // 跳过root->right
+        // 不偷父节点
+        int val2 = rob(root->left) + rob(root->right); // 考虑root的左右孩子
+        umap[root] = max(val1, val2); // umap记录一下结果
+        return max(val1, val2);
+    }
+};
+// 动态规划的思路：本题其实是树形dp入门款；重点是维护一个大小为2的数组
+// 时间复杂度：O(n) 每个节点只遍历了一次
+// 空间复杂度：O(n) 算上递推系统栈的空间
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        vector<int> result = robTree(root);
+        return max(result[0], result[1]);
+    }
+    // 长度为2的数组，0：不偷，1：偷
+    vector<int> robTree(TreeNode* cur) {
+        if (cur == NULL) return vector<int>{0, 0};
+        vector<int> left = robTree(cur->left);
+        vector<int> right = robTree(cur->right);
+        // 偷cur
+        int val1 = cur->val + left[0] + right[0];
+        // 不偷cur 
+        int val2 = max(left[0], left[1]) + max(right[0], right[1]);
+        return {val2, val1};
+    }
+};

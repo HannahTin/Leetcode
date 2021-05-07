9342d86d8497c6967f533c318ed272820ee4e63a
HannahTin <menghantin@gmail.com>
2021-05-07 16:26:22 +0800

    买卖股票的最佳时机 II（可多次买入）

---
+++ 动规/股票买卖的最佳时机/122_ Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,51 @@
+/**
+122. 买卖股票的最佳时机 II
+给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+**/
+using namespace std;
+#include <vector>
+// 同样先考虑贪心吧，「此时就是把利润分解为每天为单位的维度，而不是从0天到第3天整体去考虑！」「局部最优：收集每天的正利润，全局最优：求得最大利润」。
+/**
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices: return 0
+        res = 0
+        for i in range(1,len(prices)):
+            res += max(prices[i] - prices[i - 1], 0)
+        return res
+**/
+// dp解法
+// 时间复杂度为O(n),空间复杂度为O（n）
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        vector<vector<int>> dp(len, vector<int>(2, 0));
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]); // 注意这里是和121. 买卖股票的最佳时机唯一不同的地方。
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[len - 1][1];
+    }
+};
+//优化一下
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        vector<vector<int>> dp(2, vector<int>(2)); // 注意这里只开辟了一个2 * 2大小的二维数组
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] - prices[i]);
+            dp[i % 2][1] = max(dp[(i - 1) % 2][1], prices[i] + dp[(i - 1) % 2][0]);
+        }
+        return dp[(len - 1) % 2][1];
+    }
+};

f5553a290de63da1c62def99df44668b99e0795e
HannahTin <menghantin@gmail.com>
2021-05-08 18:11:12 +0800

    买卖股票的最佳时机 III (最多买卖两次)

---
+++ 动规/股票买卖的最佳时机/123_ Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,39 @@
+/**
+123. 买卖股票的最佳时机 III
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+**/
+using namespace std;
+#include<vector>
+// 本题相比II的区别在于最多买卖两次，II对于买卖次数没有要求。这意味着可以买卖一次，可以买卖两次，也可以不买卖。
+// 参考https://mp.weixin.qq.com/s/Sbs157mlVDtAR0gbLpdKzg
+/**
+1. 确定dp数组以及下标的含义
+一天一共就有五个状态， 0. 没有操作
+
+第一次买入
+第一次卖出
+第二次买入
+第二次卖出
+dp[i][j]中 i表示第i天，j为 [0 - 4] 五个状态，dp[i][j]表示第i天状态j所剩最大现金。
+**/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() == 0) return 0;
+        vector<vector<int>> dp(prices.size(), vector<int>(5, 0));
+        dp[0][1] = -prices[0];
+        dp[0][3] = -prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        return dp[prices.size() - 1][4];
+    }
+};

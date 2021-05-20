8358df3a27bf4f00ffe304ac8aa4950add7161aa
HannahTin <menghantin@gmail.com>
2021-05-20 15:02:48 +0800

    把数组排成最小的数 (python内置函数版)

---
+++ 剑指offer/45.py
@@ -0,0 +1,14 @@
+# python版内置函数，需要定义sort_rule
+import functools
+class Solution:
+    def minNumber(self, nums: list) -> str:
+        def sort_rule(x, y):
+            a, b = x + y, y + x
+            if a > b: return 1
+            elif a < b: return -1
+            else: return 0
+        
+        strs = [str(num) for num in nums]
+        strs.sort(key = functools.cmp_to_key(sort_rule))
+        return ''.join(strs)
+

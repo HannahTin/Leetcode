2d5aa2a7c2c7b91007f079d9618d7a56e1b6549e
HannahTin <menghantin@gmail.com>
2022-01-10 19:51:10 +0800

    完全二叉树的节点个数

--- 二叉树/222_Count Complete Tree Nodes.cpp
+++ 二叉树/222_Count Complete Tree Nodes.cpp
@@ -26,4 +26,24 @@ int countNodes(TreeNode* root) {
         int right = countNodes(root->right);
         return 1+ left+right;
 
+}
+// 针对完全二叉树的特性
+int countNodes(TreeNode* root) {
+          if (root == nullptr) return 0;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        int leftHeight = 0, rightHeight = 0; // 这里初始为0是有目的的，为了下面求指数方便
+        while (left) {  // 求左子树深度
+            left = left->left;
+            leftHeight++;
+        }
+        while (right) { // 求右子树深度
+            right = right->right;
+            rightHeight++;
+        }
+        if (leftHeight == rightHeight) {
+            return (2 << leftHeight) - 1; // 注意(2<<1) 相当于2^2，所以leftHeight初始为0
+        }
+        return countNodes(root->left) + countNodes(root->right) + 1;
+
     }

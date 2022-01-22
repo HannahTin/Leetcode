20a486191f8f13030fc00b36252127039b3820d4
HannahTin <menghantin@gmail.com>
2022-01-22 20:40:24 +0800

    找树左下角的值

--- 二叉树/513_Find Bottom Left Tree Value.cpp
+++ 二叉树/513_Find Bottom Left Tree Value.cpp
@@ -16,6 +16,7 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+//迭代写法，更新保存每一行的第一个节点值。
 int findBottomLeftValue(TreeNode* root) {
         queue<TreeNode*> que;
         que.push(root);
@@ -31,7 +32,38 @@ int findBottomLeftValue(TreeNode* root) {
 
             }
         }
-        return res;
-
-        
+        return res;     
     }
+// 递归写法，找到深度最大的叶子节点，那么如果找最左边的呢？可以使用前序遍历，这样才先优先左边搜索，然后记录深度最大的叶子节点。
+class Solution {
+private:
+    int maxLen = INT_MIN;
+    int leftvalue;
+    void traversal(TreeNode* node,int leftLen){
+        if(node->left == nullptr and node->right==nullptr){
+            if(leftLen>maxLen){
+                maxLen = leftLen;
+                leftvalue = node->val;
+            }
+            return; //中
+        }
+        if(node->left){ //左
+            leftLen ++;
+            traversal(node->left, leftLen);
+            leftLen --;
+        }
+         if(node->right){ //右
+            leftLen ++;
+            traversal(node->right, leftLen);
+            leftLen --;
+        }
+        return;
+
+
+    }
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        traversal(root,0);
+        return leftvalue;
+    }
+};

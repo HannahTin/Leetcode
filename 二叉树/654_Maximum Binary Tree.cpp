80b28847b976688204e56a411efc0353274a978c
HannahTin <menghantin@gmail.com>
2022-01-30 22:06:20 +0800

    最大二叉树

--- 二叉树/654_Maximum Binary Tree.cpp
+++ 二叉树/654_Maximum Binary Tree.cpp
@@ -25,22 +25,33 @@ struct TreeNode {
 // 第一步找到root分割点。本题是nums中的最大值。
 // 第二步就是找到待分割序列的左序列的开始结束位置，右序列的开始结束位置
 // 第三步就是单层递归逻辑
-TreeNode* constructMaximumBinaryTreeCore(vector<int>& nums,int start,int end){
-        if(end - start == 0) return nullptr;
-        int maxValueIndex = start;
-        for (int i = start + 1; i < end; ++i) {
-            if (nums[i] > nums[maxValueIndex]) maxValueIndex = i;
+class Solution {
+private:
+    int maxIndex(vector<int>& nums,int start,int final){
+        int maxVal = nums[start];
+        int index = start;
+        for(int i=start+1;i<final;i++){
+            if(nums[i]>maxVal){
+                maxVal = nums[i];
+                index = i;
+            } 
         }
-        TreeNode* root = new TreeNode(nums[maxValueIndex]);
-        if(end - start == 0) return root;
-        root->left = constructMaximumBinaryTreeCore(nums, start, maxValueIndex);
-        root->right = constructMaximumBinaryTreeCore(nums, maxValueIndex + 1, end);
-
+        return index;
+    }
+    TreeNode* traversal(vector<int>& nums,int begin,int end){
+        if(begin == end) return nullptr;
+        int index = maxIndex(nums,begin,end);
+        TreeNode* root = new TreeNode(nums[index]);
+        root->left = traversal(nums,begin,index);
+        root->right = traversal(nums,index+1,end);
         return root;
 
-}
+    }
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        if(nums.size()==0) return nullptr;
+        return traversal(nums,0,nums.size());
         
-TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return constructMaximumBinaryTreeCore(nums,0,nums.size());    
 
-}
+    }
+};

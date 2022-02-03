c53b58a42bae5d0c78eb11fc8fcd24fd8f7d810e
HannahTin <menghantin@gmail.com>
2022-02-03 17:27:46 +0800

    验证二叉搜索树

--- 二叉树/二叉搜索树/98_Validate Binary Search Tree.cpp
+++ 二叉树/二叉搜索树/98_Validate Binary Search Tree.cpp
@@ -21,13 +21,55 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+//递归:判断中序遍历的数组是否是递增数组。
+class Solution1 {
+private:
+    vector<int> vec;
+    void traversal(TreeNode* node){
+        if(node==nullptr) return;
+        traversal(node->left);
+        vec.push_back(node->val);
+        traversal(node->right);
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root==nullptr) return true;
+        traversal(root);
+        for (int i = 1; i < vec.size(); i++) {
+            if (vec[i] <= vec[i - 1]) return false;
+        }
+        return true;
 
+    }
+};
+// 递归：
+class Solution2 {
+private:
+    long long maxVal = LONG_MIN;
+    // TreeNode* pre = NULL; 
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root==nullptr) return true;
+        bool left = isValidBST(root->left);
+        // 中序遍历，验证遍历的元素是不是从小到大
+        if (maxVal < root->val) maxVal = root->val;
+        else return false;
+        // 上面两句改写为（建议避免初始化最小值，如下方法取到最左面节点的数值来比较）：
+        // if (pre != NULL && pre->val >= root->val) return false;
+        // pre = root; // 记录前一个节点
+        bool right = isValidBST(root->right);
+        return left&&right;
+
+
+    }
+};
+// 迭代模拟中序遍历
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         stack<TreeNode*> st;
         TreeNode* cur = root;
-        long inorder = (long)INT_MIN - 1;
+        TreeNode* pre = nullptr;
         while (cur != NULL || !st.empty()) {
             if (cur != NULL) { // 指针来访问节点，访问到最底层
                 st.push(cur); // 讲访问的节点放进栈
@@ -35,8 +77,11 @@ public:
             } else {
                 cur = st.top(); // 从栈里弹出的数据，就是要处理的数据（放进result数组里的数据）
                 st.pop();
-                if(cur->val <= inorder) return false; 
-                inorder = cur->val;     // 中
+                
+                if (pre != NULL && cur->val <= pre->val)
+                return false;
+                pre = cur; //保存前一个访问的结点 
+
                 cur = cur->right;               // 右
             }
         }

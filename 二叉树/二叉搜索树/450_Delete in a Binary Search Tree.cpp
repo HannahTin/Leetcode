099f7b92a56f9088c63d80bf5ee86994c01f586f
HannahTin <menghantin@gmail.com>
2022-02-05 20:52:00 +0800

    删除普通二叉树/二叉搜索树中的节点

--- 二叉树/二叉搜索树/450_Delete in a Binary Search Tree.cpp
+++ 二叉树/二叉搜索树/450_Delete in a Binary Search Tree.cpp
@@ -36,19 +36,91 @@ class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) 
     {
-        if (root == nullptr)    return nullptr;
-        if (key > root->val)    root->right = deleteNode(root->right, key);     // 去右子树删除
-        else if (key < root->val)    root->left = deleteNode(root->left, key);  // 去左子树删除
-        else    // 当前节点就是要删除的节点
-        {
-            if (! root->left)   return root->right; // 情况1，欲删除节点无左子
-            if (! root->right)  return root->left;  // 情况2，欲删除节点无右子
-            TreeNode* node = root->right;           // 情况3，欲删除节点左右子都有 
-            while (node->left)          // 寻找欲删除节点右子树的最左节点
-                node = node->left;
-            node->left = root->left;    // 将欲删除节点的左子树成为其右子树的最左节点的左子树
-            root = root->right;         // 欲删除节点的右子顶替其位置，节点被删除
+        if (root == nullptr) return root; // 第一种情况：没找到删除的节点，遍历到空节点直接返回了
+        if (root->val == key) {
+            // 第二种情况：左右孩子都为空（叶子节点），直接删除节点， 返回NULL为根节点
+            // 第三种情况：其左孩子为空，右孩子不为空，删除节点，右孩子补位 ，返回右孩子为根节点
+            if (root->left == nullptr) return root->right;
+            // 第四种情况：其右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
+            else if (root->right == nullptr) return root->left;
+            // 第五种情况：左右孩子节点都不为空，则将删除节点的左子树放到删除节点的右子树的最左面节点的左孩子的位置
+            // 并返回删除节点右孩子为新的根节点。
+            else {
+                TreeNode* cur = root->right; // 找右子树最左面的节点
+                while(cur->left != nullptr) {
+                    cur = cur->left;
+                }
+                cur->left = root->left; // 把要删除的节点（root）左子树放在cur的左孩子的位置
+                TreeNode* tmp = root;   // 把root节点保存一下，下面来删除
+                root = root->right;     // 返回旧root的右孩子作为新root
+                delete tmp;             // 释放节点内存（这里不写也可以，但C++最好手动释放一下吧）
+                return root;
+            }
         }
-        return root;    
+        if (root->val > key) root->left = deleteNode(root->left, key);
+        if (root->val < key) root->right = deleteNode(root->right, key);
+        return root;  
+    }
+};
+
+// 迭代复杂一些:，最关键就是删除节点的操作
+class Solution {
+private:
+    // 将目标节点（删除节点）的左子树放到 目标节点的右子树的最左面节点的左孩子位置上
+    // 并返回目标节点右孩子为新的根节点
+    // 是动画里模拟的过程
+    TreeNode* deleteOneNode(TreeNode* target) {
+        if (target == nullptr) return target;
+        if (target->right == nullptr) return target->left;
+        TreeNode* cur = target->right;
+        while (cur->left) {
+            cur = cur->left;
+        }
+        cur->left = target->left;
+        return target->right;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) return root;
+        TreeNode* cur = root;
+        TreeNode* pre = nullptr; // 记录cur的父节点，用来删除cur
+        while (cur) {
+            if (cur->val == key) break;
+            pre = cur;
+            if (cur->val > key) cur = cur->left;
+            else cur = cur->right;
+        }
+        if (pre == nullptr) { // 如果搜索树只有头结点
+            return deleteOneNode(cur);
+        }
+        // pre 要知道是删左孩子还是右孩子
+        if (pre->left && pre->left->val == key) {
+            pre->left = deleteOneNode(cur);
+        }
+        if (pre->right && pre->right->val == key) {
+            pre->right = deleteOneNode(cur);
+        }
+        return root;
+    }
+};
+
+// 普通二叉树的删除方式
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) return root;
+        if (root->val == key) {
+            if (root->right == nullptr) { // 这里第二次操作目标值：最终删除的作用
+                return root->left;
+            }
+            TreeNode *cur = root->right;
+            while (cur->left) {
+                cur = cur->left;
+            }
+            swap(root->val, cur->val); // 这里第一次操作目标值：交换目标值其右子树最左面节点。
+        }
+        root->left = deleteNode(root->left, key);
+        root->right = deleteNode(root->right, key);
+        return root;
     }
 };

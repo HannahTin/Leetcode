3d7a373fd0d0244addf89d06c426a0bcf7694e68
HannahTin <menghantin@gmail.com>
2022-02-07 11:06:57 +0800

    将有序数组转换为平衡二叉搜索树

--- 二叉树/二叉搜索树/108_Convert Sorted Array to Binary Search Tree.cpp
+++ 二叉树/二叉搜索树/108_Convert Sorted Array to Binary Search Tree.cpp
@@ -1,11 +1,12 @@
 /**
-108. 将有序数组转换为二叉搜索树
+108. 将有序数组转换为平衡二叉搜索树
 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
 
 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
 **/
 using namespace std;
 #include <vector>
+#include<queue>
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -14,9 +15,9 @@ struct TreeNode {
    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
- 
-
-
+//  如果根据数组构造一颗二叉树。
+// 本质就是寻找分割点，分割点作为当前节点，然后递归左区间和右区间。
+// 在构造二叉树的时候尽量不要重新定义左右区间数组，而是用下标来操作原数组。
 class Solution {
 private:
     TreeNode* dfs(vector<int>& nums,int lo,int hi){
@@ -35,5 +36,46 @@ private:
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         return dfs(nums, 0, nums.size() - 1);
+    }
+};
+// 迭代法：迭代法可以通过三个队列来模拟，一个队列放遍历的节点，一个队列放左区间下飙，一个队列放右区间下标。
+// 模拟的就是不断分割的过程
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) return nullptr;
+
+        TreeNode* root = new TreeNode(0);   // 初始根节点
+        queue<TreeNode*> nodeQue;           // 放遍历的节点
+        queue<int> leftQue;                 // 保存左区间下表
+        queue<int> rightQue;                // 保存右区间下表
+        nodeQue.push(root);                 // 根节点入队列
+        leftQue.push(0);                    // 0为左区间下表初始位置
+        rightQue.push(nums.size() - 1);     // nums.size() - 1为右区间下表初始位置
+
+        while (!nodeQue.empty()) {
+            TreeNode* curNode = nodeQue.front();
+            nodeQue.pop();
+            int left = leftQue.front(); leftQue.pop();
+            int right = rightQue.front(); rightQue.pop();
+            int mid = left + ((right - left) / 2);
+
+            curNode->val = nums[mid];       // 将mid对应的元素给中间节点
+
+            if (left <= mid - 1) {          // 处理左区间
+                curNode->left = new TreeNode(0);
+                nodeQue.push(curNode->left);
+                leftQue.push(left);
+                rightQue.push(mid - 1);
+            }
+
+            if (right >= mid + 1) {         // 处理右区间
+                curNode->right = new TreeNode(0);
+                nodeQue.push(curNode->right);
+                leftQue.push(mid + 1);
+                rightQue.push(right);
+            }
+        }
+        return root;
     }
 };

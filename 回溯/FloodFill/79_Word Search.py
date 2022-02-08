ff7e36fe4c944dd0fe8199f0b2b120fe493cb21e
HannahTin <menghantin@gmail.com>
2022-02-08 19:13:19 +0800

    单词搜索

--- 回溯/FloodFill/79_Word Search.py
+++ 回溯/FloodFill/79_Word Search.py
@@ -19,3 +19,42 @@ class Solution:
                 if(dfs(i,j,0)): return True
         return False
 
+# c++写法
+'''
+class Solution {
+public:
+    bool check(vector<vector<char>>& board, vector<vector<int>>& visited, int i, int j, string& s, int k) {
+        if (board[i][j] != s[k]) {
+            return false;
+        } else if (k == s.length() - 1) {
+            return true;
+        }
+        visited[i][j] = true;
+        vector<pair<int, int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (const auto& dir: directions) {
+            int newi = i + dir.first, newj = j + dir.second;
+            if (newi >= 0 && newi < board.size() && newj >= 0 && newj < board[0].size()) {
+                if (!visited[newi][newj] and check(board, visited, newi, newj, s, k + 1)) {
+                    return true;
+                }
+            }
+        }
+        visited[i][j] = false;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int h = board.size(), w = board[0].size();
+        vector<vector<int>> visited(h, vector<int>(w));
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                bool flag = check(board, visited, i, j, word, 0);
+                if (flag) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+'''

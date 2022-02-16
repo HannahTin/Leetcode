0746c70f4fa9fa4f44aa8f827d6ac7f9f92631fb
HannahTin <menghantin@gmail.com>
2022-02-17 07:56:06 +0800

    反转字符串 II

---
+++ 数组/双指针/字符串中的双指针/541_Reverse String II.cpp
@@ -0,0 +1,53 @@
+/*
+541. 反转字符串 II
+
+给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ 
+
+示例 1：
+
+输入：s = "abcdefg", k = 2
+输出："bacdfeg"
+示例 2：
+
+输入：s = "abcd", k = 2
+输出："bacd"
+*/
+using namespace std;
+#include<string>
+// 我们直接按题意进行模拟：反转每个下标从 2k 的倍数开始的，长度为 k 的子串。若该子串长度不足 k，则反转整个子串。
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
+        }
+        return s;
+    }
+};
+// 如果不用库函数，那就是双指针原地交换即可
+class Solution {
+public:
+    void reverse(string& s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            // 1. 每隔 2k 个字符的前 k 个字符进行反转
+            // 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            if (i + k <= s.size()) {
+                reverse(s, i, i + k - 1);
+                continue;
+            }
+            // 3. 剩余字符少于 k 个，则将剩余字符全部反转。
+            reverse(s, i, s.size() - 1);
+        }
+        return s;
+    }
+};

20db99181e7358a952419b0fb7690cd95859ed94
HannahTin <menghantin@gmail.com>
2022-02-27 16:42:09 +0800

    删除排序链表中的重复元素(每个元素只出现一次)

---
+++ 链表/删除链表/83_Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,35 @@
+/*
+83. 删除排序链表中的重复元素
+给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+
+输入：head = [1,1,2]
+输出：[1,2]
+*/
+
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) {
+            return head;
+        }
+        ListNode* cur = head;
+        while (cur->next) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+
+        return head;
+    }
+};

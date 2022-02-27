f3ca96d0bf1d3169deef9dbceae16043f47e4dfa
HannahTin <menghantin@gmail.com>
2022-02-27 16:41:48 +0800

    删除排序链表中的重复元素 II(只留下不同的数字)

---
+++ 链表/删除链表/82_Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,49 @@
+/*
+82. 删除排序链表中的重复元素 II
+给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+输入：head = [1,2,3,3,4,4,5]
+输出：[1,2,5]
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) {
+            return head;
+        }
+        
+        ListNode* dummy = new ListNode(0, head);
+
+        ListNode* cur = dummy;
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                int x = cur->next->val;
+                while (cur->next && cur->next->val == x) {
+                    cur->next = cur->next->next;
+                }
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+
+        return dummy->next;
+    }
+};

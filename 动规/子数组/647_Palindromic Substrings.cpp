c403e5af1ee10eed7a943bbd46a6a44ec2d85365
HannahTin <menghantin@gmail.com>
2022-03-16 10:30:34 +0800

    回文子串

---
+++ 动规/子数组/647_Palindromic Substrings.cpp
@@ -0,0 +1,38 @@
+/**
+647. 回文子串
+给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+ 
+
+示例 1：
+
+输入："abc"
+输出：3
+解释：三个回文子串: "a", "b", "c"
+**/
+using namespace std;
+#include<string>
+#include<vector>
+class Solution {
+public:
+    int countSubstrings(string s) {
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int result = 0;
+        for (int i = s.size() - 1; i >= 0; i--) {  // 注意遍历顺序
+            for (int j = i; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    if (j - i <= 1) { // 情况一 和 情况二
+                        result++;
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) { // 情况三
+                        result++;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};

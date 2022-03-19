f1424852f8c9182fe063f6125ecdc0bd323708f6
HannahTin <menghantin@gmail.com>
2022-03-19 12:25:26 +0800

    零钱兑换

--- 动规/零钱兑换/322_Coin Change.cpp
+++ 动规/零钱兑换/322_Coin Change.cpp
@@ -6,6 +6,32 @@
 **/
 using namespace std;
 #include <vector>
+// bfs,主要是对used数组的理解，如果当前计算的余额已经在之前层计算过，那就无需再加入队列中计算。
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        queue<int> que;
+        que.push(amount);
+        set<int> used;
+        int layer =0;
+        while(!que.empty()){
+            int sz = que.size();
+            for(int i=0;i<sz;i++){
+                int curr = que.front();
+                que.pop();
+                if(curr==0) return layer;
+                for(int j=0;j<coins.size();j++){
+                    if(curr<coins[j] || used.count(curr-coins[j])) continue;
+                    used.insert(curr-coins[j]);
+                    que.push(curr-coins[j]);
+                }
+            }
+            layer++;
+        }
+        return -1;
+
+    }
+};
 // 完全背包变体：物品的重量就是硬币的面额，物品的价值也是硬币的面额，背包容量就是总金额
 class Solution {
 public:
